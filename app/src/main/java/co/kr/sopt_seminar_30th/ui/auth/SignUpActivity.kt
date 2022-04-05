9553d7e8f95c5653233a209ce596471f4489b19d
Seunghyeon Han <hansh0101@naver.com>
2022-04-05 16:12:19 +0900

    [UI] #1 : 회원가입 화면 UI 구현

---
+++ app/src/main/java/co/kr/sopt_seminar_30th/ui/auth/SignUpActivity.kt
@@ -0,0 +1,18 @@
+package co.kr.sopt_seminar_30th.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.kr.sopt_seminar_30th.R
+import co.kr.sopt_seminar_30th.databinding.ActivitySignUpBinding
+import co.kr.sopt_seminar_30th.ui.base.BaseActivity
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>() {
+    override val TAG: String
+        get() = SignUpActivity::class.java.simpleName
+    override val layoutRes: Int
+        get() = R.layout.activity_sign_up
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
