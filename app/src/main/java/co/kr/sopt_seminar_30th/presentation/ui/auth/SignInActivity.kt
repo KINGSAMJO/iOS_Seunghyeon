fccc14eb4a84c1aebfd8dbf0b1a37ecb2650d114
Seunghyeon Han <hansh0101@naver.com>
2022-04-06 00:47:50 +0900

    [FEAT] #1 : Room 연동한 로그인 구현

--- app/src/main/java/co/kr/sopt_seminar_30th/presentation/ui/auth/SignInActivity.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/presentation/ui/auth/SignInActivity.kt
@@ -2,16 +2,24 @@ package co.kr.sopt_seminar_30th.presentation.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import co.kr.sopt_seminar_30th.R
 import co.kr.sopt_seminar_30th.databinding.ActivitySignInBinding
 import co.kr.sopt_seminar_30th.presentation.ui.base.BaseActivity
+import co.kr.sopt_seminar_30th.presentation.ui.home.HomeActivity
+import co.kr.sopt_seminar_30th.presentation.viewmodel.SignInViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignInActivity : BaseActivity<ActivitySignInBinding>() {
     override val TAG: String
         get() = SignInActivity::class.java.simpleName
     override val layoutRes: Int
         get() = R.layout.activity_sign_in
+
+    private val signInViewModel by viewModels<SignInViewModel>()
 
     private val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -23,13 +31,38 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.viewmodel = signInViewModel
+        binding.lifecycleOwner = this
         signUp()
+        login()
+        observeLogin()
     }
 
     private fun signUp() {
         binding.btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             activityResultLauncher.launch(intent)
+        }
+    }
+
+    private fun login() {
+        binding.btnSignIn.setOnClickListener {
+            signInViewModel.checkSignInPermission()
+        }
+    }
+
+    private fun observeLogin() {
+        signInViewModel.isSuccess.observe(this) {
+            when(it) {
+                true -> {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else -> {
+                    Toast.makeText(this, "다시 시도해주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
