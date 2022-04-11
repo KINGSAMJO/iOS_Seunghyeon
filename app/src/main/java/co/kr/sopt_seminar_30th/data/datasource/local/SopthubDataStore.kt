f74df42a973106eea555f1ccdbdf18e435e9005e
Seunghyeon Han <hansh0101@naver.com>
2022-04-11 09:57:51 +0900

    [FIX] #1 : Debug, Release 시 다른 SharedPreference 사용하도록 처리

--- app/src/main/java/co/kr/sopt_seminar_30th/data/datasource/local/SopthubDataStore.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/data/datasource/local/SopthubDataStore.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.core.content.edit
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
+import co.kr.sopt_seminar_30th.BuildConfig
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -12,13 +13,18 @@ const val FILE_NAME = "SOPTHUB_DATA_STORE"
 class SopthubDataStore @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
-    private val dataStore = EncryptedSharedPreferences.create(
-        FILE_NAME,
-        MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
-        context,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
+    private val dataStore =
+        if (BuildConfig.DEBUG) {
+            context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        } else {
+            EncryptedSharedPreferences.create(
+                FILE_NAME,
+                MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
+                context,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+        }
 
     var userId: String
         set(value) = dataStore.edit { putString("USER_ID", value) }
