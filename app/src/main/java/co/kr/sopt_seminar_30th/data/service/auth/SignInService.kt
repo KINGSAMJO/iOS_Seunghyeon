ed8f1184c076dff719dc9aa89de7aed594d189a2
hansh0101 <hansh0101@naver.com>
2022-06-09 16:56:20 +0900

    [FEAT] #7 : 4차 세미나 과제 완료 후 코드리뷰 반영

--- app/src/main/java/co/kr/sopt_seminar_30th/data/service/auth/SignInService.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/data/service/auth/SignInService.kt
@@ -1,15 +1,14 @@
 package co.kr.sopt_seminar_30th.data.service.auth
 
-import co.kr.sopt_seminar_30th.data.model.request.signin.RequestSignIn
+import co.kr.sopt_seminar_30th.data.model.request.signin.SignInRequest
 import co.kr.sopt_seminar_30th.data.model.response.BaseResponse
 import co.kr.sopt_seminar_30th.domain.entity.auth.user.UserInformation
-import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface SignInService {
     @POST("/auth/signin")
     suspend fun postSignIn(
-        @Body requestBody: RequestSignIn
+        @Body requestBody: SignInRequest
     ): BaseResponse<UserInformation>
 }
