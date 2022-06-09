9af4d13f06ef228d5210f5c64706f1226cc80628
hansh0101 <hansh0101@naver.com>
2022-06-09 16:55:19 +0900

    [FEAT] #7 : 4차 세미나 과제 완료 후 코드리뷰 반영

--- app/src/main/java/co/kr/sopt_seminar_30th/data/service/home/HomeService.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/data/service/home/HomeService.kt
@@ -1,8 +1,8 @@
 package co.kr.sopt_seminar_30th.data.service.home
 
-import co.kr.sopt_seminar_30th.data.model.response.home.ResponseFetchUserFollowItem
-import co.kr.sopt_seminar_30th.data.model.response.home.ResponseFetchUserInformation
-import co.kr.sopt_seminar_30th.data.model.response.home.ResponseFetchUserRepositoryItem
+import co.kr.sopt_seminar_30th.data.model.response.home.UserFollowResponse
+import co.kr.sopt_seminar_30th.data.model.response.home.UserInformationResponse
+import co.kr.sopt_seminar_30th.data.model.response.home.UserRepositoryResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,22 +11,22 @@ interface HomeService {
     @GET("/users/{userId}")
     suspend fun fetchUserInformation(
         @Path("userId") userId: String
-    ): ResponseFetchUserInformation
+    ): UserInformationResponse
 
     @GET("/users/{userId}/followers")
     suspend fun fetchUserFollowers(
         @Path("userId") userId: String,
         @Query("per_page") perPage: Int
-    ): List<ResponseFetchUserFollowItem>
+    ): List<UserFollowResponse>
 
     @GET("/users/{userId}/following")
     suspend fun fetchUserFollowing(
         @Path("userId") userId: String,
         @Query("per_page") perPage: Int
-    ): List<ResponseFetchUserFollowItem>
+    ): List<UserFollowResponse>
 
     @GET("/users/{userId}/repos")
     suspend fun fetchUserRepositories(
         @Path("userId") userId: String
-    ): List<ResponseFetchUserRepositoryItem>
+    ): List<UserRepositoryResponse>
 }
