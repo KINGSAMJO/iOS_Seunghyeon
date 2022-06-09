3c66fcc36681f13f810e917962802f936a10c391
hansh0101 <hansh0101@naver.com>
2022-06-09 16:56:15 +0900

    [FEAT] #7 : 4차 세미나 과제 완료 후 코드리뷰 반영

--- app/src/main/java/co/kr/sopt_seminar_30th/data/repositoryimpl/remote/SignInRepositoryImpl.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/data/repositoryimpl/remote/SignInRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package co.kr.sopt_seminar_30th.data.repositoryimpl.remote
 
-import co.kr.sopt_seminar_30th.data.model.request.signin.RequestSignIn
+import co.kr.sopt_seminar_30th.data.model.request.signin.SignInRequest
 import co.kr.sopt_seminar_30th.data.service.auth.SignInService
 import co.kr.sopt_seminar_30th.domain.entity.auth.user.UserInformation
 import co.kr.sopt_seminar_30th.domain.repository.remote.SignInRepository
@@ -12,11 +12,10 @@ class SignInRepositoryImpl @Inject constructor(
     private val service: SignInService,
     private val coroutineDispatcher: CoroutineDispatcher
 ) : SignInRepository {
-    override suspend fun signIn(email: String, password: String): Result<UserInformation> {
-        return withContext(coroutineDispatcher) {
+    override suspend fun signIn(email: String, password: String): Result<UserInformation> =
+        withContext(coroutineDispatcher) {
             kotlin.runCatching {
-                service.postSignIn(RequestSignIn(email, password)).data
+                service.postSignIn(SignInRequest(email, password)).data
             }
         }
-    }
 }
