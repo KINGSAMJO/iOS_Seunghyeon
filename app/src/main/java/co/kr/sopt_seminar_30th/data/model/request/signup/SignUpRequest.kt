da02afe0a29604af2d95520542d76cb17d89a027
hansh0101 <hansh0101@naver.com>
2022-06-09 16:56:24 +0900

    [FEAT] #7 : 4차 세미나 과제 완료 후 코드리뷰 반영

--- app/src/main/java/co/kr/sopt_seminar_30th/data/model/request/signup/SignUpRequest.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/data/model/request/signup/SignUpRequest.kt
@@ -1,6 +1,6 @@
 package co.kr.sopt_seminar_30th.data.model.request.signup
 
-data class RequestSignUp(
+data class SignUpRequest(
     val name: String,
     val email: String,
     val password: String
