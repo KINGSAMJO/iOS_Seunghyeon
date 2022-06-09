c0f409758951e1a9be002b666ef1859d477c862b
hansh0101 <hansh0101@naver.com>
2022-06-09 16:55:11 +0900

    [FEAT] #7 : 4차 세미나 과제 완료 후 코드리뷰 반영

--- app/src/main/java/co/kr/sopt_seminar_30th/domain/repository/remote/HomeRepository.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/domain/repository/remote/HomeRepository.kt
@@ -1,12 +1,12 @@
 package co.kr.sopt_seminar_30th.domain.repository.remote
 
-import co.kr.sopt_seminar_30th.domain.entity.home.UserFollowInformation
-import co.kr.sopt_seminar_30th.domain.entity.home.UserProfileInformation
-import co.kr.sopt_seminar_30th.domain.entity.home.UserRepositoryInformation
+import co.kr.sopt_seminar_30th.domain.entity.home.UserFollow
+import co.kr.sopt_seminar_30th.domain.entity.home.UserProfile
+import co.kr.sopt_seminar_30th.domain.entity.home.UserRepository
 
 interface HomeRepository {
-    suspend fun fetchUserInformation(userId: String): Result<UserProfileInformation>
-    suspend fun fetchUserFollowers(userId: String): Result<List<UserFollowInformation>>
-    suspend fun fetchUserFollowing(userId: String): Result<List<UserFollowInformation>>
-    suspend fun fetchUserRepositories(userId: String): Result<List<UserRepositoryInformation>>
+    suspend fun fetchUserInformation(userId: String): Result<UserProfile>
+    suspend fun fetchUserFollowers(userId: String): Result<List<UserFollow>>
+    suspend fun fetchUserFollowing(userId: String): Result<List<UserFollow>>
+    suspend fun fetchUserRepositories(userId: String): Result<List<UserRepository>>
 }
