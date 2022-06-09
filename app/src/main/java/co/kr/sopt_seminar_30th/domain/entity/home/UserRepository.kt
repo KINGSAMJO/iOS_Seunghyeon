14f65258d75420a0fce35fe871614b5f51ef42d5
hansh0101 <hansh0101@naver.com>
2022-06-09 16:56:40 +0900

    [FEAT] #7 : 4차 세미나 과제 완료 후 코드리뷰 반영

--- app/src/main/java/co/kr/sopt_seminar_30th/domain/entity/home/UserRepository.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/domain/entity/home/UserRepository.kt
@@ -1,6 +1,6 @@
 package co.kr.sopt_seminar_30th.domain.entity.home
 
-data class UserRepositoryInformation(
+data class UserRepository(
     val name: String,
     val language: String,
     var repositoryOrder: Int
