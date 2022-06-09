23641ee475291c7b3e047767e598f63cdc335c14
hansh0101 <hansh0101@naver.com>
2022-06-09 16:56:28 +0900

    [FEAT] #7 : 4차 세미나 과제 완료 후 코드리뷰 반영

--- app/src/main/java/co/kr/sopt_seminar_30th/data/service/auth/SignUpService.kt
+++ app/src/main/java/co/kr/sopt_seminar_30th/data/service/auth/SignUpService.kt
@@ -1,14 +1,14 @@
 package co.kr.sopt_seminar_30th.data.service.auth
 
-import co.kr.sopt_seminar_30th.data.model.request.signup.RequestSignUp
+import co.kr.sopt_seminar_30th.data.model.request.signup.SignUpRequest
 import co.kr.sopt_seminar_30th.data.model.response.BaseResponse
-import co.kr.sopt_seminar_30th.data.model.response.signup.ResponseSignUp
+import co.kr.sopt_seminar_30th.data.model.response.signup.SignUpResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface SignUpService {
     @POST("/auth/signup")
     suspend fun postSignUp(
-        @Body requestBody: RequestSignUp
-    ): BaseResponse<ResponseSignUp>
+        @Body requestBody: SignUpRequest
+    ): BaseResponse<SignUpResponse>
 }
